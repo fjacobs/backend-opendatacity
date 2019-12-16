e38e529b86064a1b17eec9314e6da1a420c79d51
Ferdinand Jacobs <jacobsfer@hotmail.com>
2019-12-16 16:30:15 +0100

    Detach frontend from this repo

--- src/frontend/static/googlemapskey.js
+++
@@ -1,1 +0,0 @@
-https://maps.googleapis.com/maps/api/js?key=AIzaSyB6SSvjmmzWA9zOVHhh4IsBbp3qqY25qas&callback=initMap
--- src/main/resources/static/gulpfile.js
+++
@@ -1,6 +0,0 @@
-function defaultTask(cb) {
-    browserify().transform("babelify", {presets: ["es2015"]});
-    cb();
-}
-
-exports.default = defaultTask
